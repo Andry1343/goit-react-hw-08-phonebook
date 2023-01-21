b260524ebc8e875c0df35043b9223a0f12cf9095
Andry1343 <ua.gavrilets@gmail.com>
2023-01-22 00:33:33 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { ContactForm } from '../components/form/Form.jsx';
 import { Filter } from '../components/filter/Filter.jsx';
 import { ContactList } from '../components/contactList/ContactList.jsx';
+
+/* import { AuthNav } from './authNav/AuthNav.jsx'; */
+
 import { fetchContacts } from './redux/operations.js';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
@@ -17,7 +20,9 @@ export const App = () => {
   }, [dispatch]);
 
   return (
+    
     <div className={style.form}>
+      {/* <AuthNav></AuthNav> */}
       <div className={style.form_name_number_filter}>
         <h1>Phonebook</h1>
         <ContactForm></ContactForm>
